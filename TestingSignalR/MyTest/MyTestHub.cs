73b9f7f99411a747766fc35dc22aa18b02369ef1
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2013-04-07 07:21:47 -0500

    Initial version testing ignore

---
+++ TestingSignalR/MyTest/MyTestHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TestingSignalR.MyTest
+{
+    public class MyTestHub : Hub
+    {
+        public void Test(string message) {
+
+            //Clients.Others.messageSended(string message);
+
+        }
+    }
+}
